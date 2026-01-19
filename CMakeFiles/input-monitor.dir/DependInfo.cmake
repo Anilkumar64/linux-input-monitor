8600b1120a6def3fca9712f4456402957768badd
Anilkumar64 <vennapureddyanil456@gmail.com>
2026-01-19 20:53:10 +0530

    finished input monitor from mouse

---
+++ CMakeFiles/input-monitor.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/anilreddy/Documents/C++/linux input montior/src/device.cpp" "CMakeFiles/input-monitor.dir/src/device.cpp.o" "gcc" "CMakeFiles/input-monitor.dir/src/device.cpp.o.d"
+  "/home/anilreddy/Documents/C++/linux input montior/src/event_printer.cpp" "CMakeFiles/input-monitor.dir/src/event_printer.cpp.o" "gcc" "CMakeFiles/input-monitor.dir/src/event_printer.cpp.o.d"
+  "/home/anilreddy/Documents/C++/linux input montior/src/event_reader.cpp" "CMakeFiles/input-monitor.dir/src/event_reader.cpp.o" "gcc" "CMakeFiles/input-monitor.dir/src/event_reader.cpp.o.d"
+  "/home/anilreddy/Documents/C++/linux input montior/src/main.cpp" "CMakeFiles/input-monitor.dir/src/main.cpp.o" "gcc" "CMakeFiles/input-monitor.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/input-monitor.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/input-monitor.dir/src/device.cpp.o"
+  "CMakeFiles/input-monitor.dir/src/device.cpp.o.d"
+  "CMakeFiles/input-monitor.dir/src/event_printer.cpp.o"
+  "CMakeFiles/input-monitor.dir/src/event_printer.cpp.o.d"
+  "CMakeFiles/input-monitor.dir/src/event_reader.cpp.o"
+  "CMakeFiles/input-monitor.dir/src/event_reader.cpp.o.d"
+  "CMakeFiles/input-monitor.dir/src/main.cpp.o"
+  "CMakeFiles/input-monitor.dir/src/main.cpp.o.d"
+  "input-monitor"
+  "input-monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/input-monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
